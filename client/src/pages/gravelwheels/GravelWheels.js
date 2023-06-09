e94ea9dad277d40b74da3ef4e21033b118f8fcf0
Heidrun Kubiessa <heidrun.kubiessa@utah.edu>
2023-06-09 16:59:04 -0600

    fixed errors in backend schema

--- client/src/pages/gravelwheels/GravelWheels.js
+++ client/src/pages/gravelwheels/GravelWheels.js
@@ -1,12 +1,18 @@
-import React from "react";
+import{ React, useState } from "react";
+import Cart from "../../components/cart/Cart";
 
 //import "./About.css";
 //import About from "../about/About";
 
 const GravelWheels = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  function toggleDropdown() {
+    setShowDropdown(!showDropdown);
+  }
+
   return (
     <>
-      
+       <Cart/>
     </>
   );
 };
