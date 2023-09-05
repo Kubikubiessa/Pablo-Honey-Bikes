08a9a31aa1191d481e5941a3067e9fe79d695807
Heidrun Kubiessa <heidrun.kubiessa@utah.edu>
2023-09-05 13:53:44 -0600

    further tested mutation routes, addORder works,  and fixed update and delete routes for category and product

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require("apollo-server-express");
-const bcrypt = require('bcrypt'); 
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const OrderItem = require("../models/OrderItem"); // Import statement for the OrderItem type
 const Role = require("../models/Role");
@@ -8,9 +8,9 @@ const Category = require("../models/Category");
 const Order = require("../models/Order");
 const { signToken } = require("../utils/auth");
 const { authMiddleware } = require("../utils/auth");
-const { requireAuth } = require("../utils/auth"); 
-const { getRoleByName } = require ("../utils/getRoleByName")
- 
+const { requireAuth } = require("../utils/auth");
+const { getRoleByName } = require("../utils/getRoleByName");
+
 //const { checkAuthorization } = require("../utils/auth"); // Import  requireAuth function
 const fetch = require("node-fetch");
 
@@ -39,7 +39,9 @@ const resolvers = {
     },
     user: async (parent, { _id }, context, info) => {
       try {
-        const user = await User.findById(_id).populate("role").populate("orders");
+        const user = await User.findById(_id)
+          .populate("role")
+          .populate("orders");
         return user;
       } catch (error) {
         console.error("user:", error);
@@ -107,96 +109,109 @@ const resolvers = {
   },
 
   Mutation: {
-    
-    addUser: async (parent, { username, email, password, desiredRole }, context) => {
-     
-        const desiredRoleName = await getRoleByName(desiredRole); // I implemented this function in separate file in utils folder
-        // Checking if the authenticated user has the necessary permission to create an admin account
-        if (desiredRoleName == "Admin" // admin name instead of previous admin ObjectID, which is more dynamic, using hard-coded IDs require DB work when transitioning to another DB.
-         && !context.user || context.user.role.name !== "Admin") {
-          throw new Error("You don't have the necessary permissions to create this user.");
-        }
-    
-        const hashedPassword = await bcrypt.hash(password, 10);
-    
-        // Retrieve the ObjectId reference of the desired Role
-       
-        const user = await User.create({
-          username,
-          email,
-          password: hashedPassword,
-          role: desiredRoleName,
-        });
-    
-        const token = signToken(user);
-        return { token, user };
-      
-    },
-    
-    addProduct: requireAuth('add_product', async (
+    addUser: async (
       parent,
-      {
-        productname,
-        description,
-        price,
-        size,
-        width,
-        weight,
-        drill,
-        categoryId,
-      },
-      context,
-      info
+      { username, email, password, desiredRole },
+      context
     ) => {
-      console.log('context:', context)
-      try {
-        const userData = context.user;
-        const userRole = userData.role;
-        console.log('user data and role:', userData, userRole)
-        if (!userData|| !userRole) {
-          throw new AuthenticationError("Not authenticated");
-        }
-    
-        const category = await Category.findById(categoryId);
-        if (!category) {
-          throw new Error("Category not found");
-        }
-        const product = await Product.create({
-                productname,
-                description,
-                price,
-                size,
-                width,
-                weight,
-                drill,
-                category: category._id,
-              });
-              return product;
-            } catch (error) {
-              console.error("addProduct :", error);
-              throw error;
-            }
-    
-       
-       
-    }),
-    
- 
-
-    addCategory: requireAuth('add_category', async (parent, { categoryname }, context, info) => {
-      try {
-         
-        // Use the authMiddlware middleware
-        // authMiddleware(requiredScope)(parent, args, context);
-        const category = await Category.create({
-          categoryname,
-        });
-        return category;
-      } catch (error) {
-        console.error("addCategory :", error); //defensive programming
-        throw error;
+      const desiredRoleName = await getRoleByName(desiredRole); // I implemented this function in separate file in utils folder
+      // Checking if the authenticated user has the necessary permission to create an admin account
+      if (
+        (desiredRoleName == "Admin" && // admin name instead of previous admin ObjectID, which is more dynamic, using hard-coded IDs require DB work when transitioning to another DB.
+          !context.user) ||
+        context.user.role.name !== "Admin"
+      ) {
+        throw new Error(
+          "You don't have the necessary permissions to create this user."
+        );
       }
-    }),
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Retrieve the ObjectId reference of the desired Role
+
+      const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+        role: desiredRoleName,
+      });
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addProduct: requireAuth(
+      "add_product",
+      async (
+        parent,
+        {
+          productname,
+          description,
+          price,
+          size,
+          width,
+          weight,
+          drill,
+          categoryId,
+        },
+        context,
+        info
+      ) => {
+        //console.log('context:', context)
+        try {
+          const userData = context.user;
+          const userRole = userData.role;
+          console.log("user data and role:", userData, userRole);
+          if (!userData || !userRole) {
+            throw new AuthenticationError("Not authenticated");
+          }
+
+          const category = await Category.findById(categoryId);
+          if (!category) {
+            throw new Error("Category not found");
+          }
+          const product = await Product.create({
+            productname,
+            description,
+            price,
+            size,
+            width,
+            weight,
+            drill,
+            category: category._id,
+          });
+          return product;
+        } catch (error) {
+          console.error("addProduct :", error);
+          throw error;
+        }
+      }
+    ),
+
+    addCategory: requireAuth(
+      "add_category",
+      async (parent, { categoryname }, context, info) => {
+        try {
+          const userData = context.user;
+          const userRole = userData.role;
+          console.log("user data and role:", userData, userRole);
+          if (!userData || !userRole) {
+            throw new AuthenticationError("Not authenticated");
+          }
+
+          // Use the authMiddlware middleware
+          // authMiddleware(requiredScope)(parent, args, context);
+          const category = await Category.create({
+            categoryname,
+          });
+          return category;
+        } catch (error) {
+          console.error("addCategory :", error); //defensive programming
+          throw error;
+        }
+      }
+    ),
 
     addOrder: async (parent, { items, status, userId }, context, info) => {
       try {
@@ -209,7 +224,6 @@ const resolvers = {
             });
           })
         );
-       
 
         const total = items.reduce(async (acc, curr) => {
           const { price } = await Product.findById(curr.productId);
@@ -227,7 +241,7 @@ const resolvers = {
           status,
           total,
           user: user._id, // Convert user ID to ObjectId
-        }); 
+        });
 
         // Update the order reference in the User document
         await User.findByIdAndUpdate(user._id, {
@@ -240,7 +254,6 @@ const resolvers = {
         throw error;
       }
     },
- 
 
     //all Update processes of the database
     updateOrder: async (parent, { _id, items, status }, context, info) => {
@@ -287,56 +300,119 @@ const resolvers = {
         throw error;
       }
     },
-
-    updateProduct: requireAuth('update_product', async (
-      parent,
-      {
-        _id,
-        productname,
-        description,
-        price,
-        width,
-        weight,
-        drill,
-        categoryId,
-      },
-      context,
-      info
-    ) => {
-      const updatedProduct = await Product.findByIdAndUpdate(
+    updateProduct: requireAuth(
+      "update_product",
+      async (
+        parent,
         {
           _id,
           productname,
           description,
           price,
+          size,
           width,
           weight,
           drill,
           categoryId,
         },
+        context,
+        info
+      ) => {
+        try {
+          const userData = context.user;
+          const userRole = userData.role;
+          console.log("user data and role:", userData, userRole);
+          if (!userData || !userRole) {
+            throw new AuthenticationError("Not authenticated");
+          }
 
-        { new: true }
-      );
-      return updatedProduct;
-    }),
-    updateCategory: requireAuth('update_category',async (
-      parent,
-      { _id, categoryname, products },
-      context,
-      info
-    ) => {
-      try {
-        const updatedCategory = await Category.findByIdAndUpdate(
-          _id,
-          { categoryname, products },
-          { new: true }
-        );
-        return updatedCategory;
-      } catch (error) {
-        console.error("updateCategory:", error);
-        throw error;
+          // Create an object with the fields to update
+          const updateFields = {};
+
+          if (productname !== undefined) {
+            updateFields.productname = productname;
+          }
+
+          if (description !== undefined) {
+            updateFields.description = description;
+          }
+
+          if (price !== undefined) {
+            updateFields.price = price;
+          }
+           if (size !== undefined) {
+            updateFields.size = size
+           }
+
+          if (width !== undefined) {
+            updateFields.width = width;
+          }
+          if (weight !== undefined) {
+            updateFields.weight = weight;
+          }
+          if (drill !== undefined) {
+            updateFields.drill = drill
+          }
+
+          // categoryId should be handled separately, as it might be null
+
+          if (categoryId !== undefined) {
+            updateFields.category = categoryId; 
+          } else if (categoryId === null) {
+            // If categoryId is explicitly set to null in the mutation, clear it
+            updateFields.category = null;
+          }
+
+          const updatedProduct = await Product.findByIdAndUpdate(
+            { _id },
+            updateFields,
+            { new: true }
+          );
+
+          console.log("updated product", updatedProduct);
+          return updatedProduct;
+        } catch (error) {
+          console.error("updated product :", error);
+          throw error;
+        }
       }
-    }),
+    ),
+
+    updateCategory: requireAuth(
+      "update_category",
+      async (parent, { _id, categoryname, products }, context, info) => {
+        try {
+          const userData = context.user;
+          const userRole = userData.role;
+          console.log("user data and role:", userData, userRole);
+          if (!userData || !userRole) {
+            throw new AuthenticationError("Not authenticated");
+          }
+          // Create an object with the fields to update
+          const updateFields = {
+            categoryname,
+            products,
+          };
+
+          // Remove undefined or null fields from the object
+          Object.keys(updateFields).forEach((key) => {
+            if (updateFields[key] === undefined || updateFields[key] === null) {
+              delete updateFields[key];
+            }
+          });
+          //updating category with the respective fields
+          const updatedCategory = await Category.findByIdAndUpdate(
+            _id,
+            updateFields,
+            { new: true }
+          );
+          return updatedCategory;
+        } catch (error) {
+          console.error("updateCategory:", error);
+          throw error;
+        }
+      }
+    ),
 
     // All delete processes of the database
 
@@ -354,34 +430,54 @@ const resolvers = {
       }
     },
 
-    deleteProduct: requireAuth('delete_product',async (parent, { _id }) => {
-      try {
-        
-        const deletedProduct = await Product.findOneAndDelete(_id);
+    deleteProduct: requireAuth(
+      "delete_product",
+      async (parent, { _id }, context, info) => {
+        try {
+          const userData = context.user;
+          const userRole = userData.role;
+          console.log("user data and role:", userData, userRole);
+          if (!userData || !userRole) {
+            throw new AuthenticationError("Not authenticated");
+          }
+          const deletedProduct = await Product.findOneAndDelete({ _id: _id });
+          const categoryId = deletedProduct.category;
 
-        await Category.findOneAndUpdate(
-          { product: deletedProduct._id },
-          { $pull: { product: deletedProduct._id } }
-        );
-        return deletedProduct;
-      } catch (error) {
-        console.error("deletedProduct:", error);
-        throw error;
+          const updatedCategory = await Category.findOneAndUpdate(
+            { _id: categoryId},
+            { $pull: { products: deletedProduct._id } }
+          );
+          console.log("product deleted :", deletedProduct._id);
+          return updatedCategory;
+        } catch (error) {
+          console.error("deletedProduct:", error);
+          throw error;
+        }
       }
-    }),
-    deleteCategory: requireAuth('delete_category',async (parent, { _id }) => {
-      try {
-        const deletedCategory = await Category.findOneAndDelete(_id);
-        await Product.updateMany(
-          { category: deletedCategory._id },
-          { category: null }
-        );
-        return deletedCategory;
-      } catch (error) {
-        console.error("deletedCategory:", error);
-        throw error;
+    ),
+    deleteCategory: requireAuth(
+      "delete_category",
+      async (parent, { _id, products }, context, info) => {
+        try {
+          const userData = context.user;
+          const userRole = userData.role;
+          // console.log("user data and role:", userData, userRole);
+          if (!userData || !userRole) {
+            throw new AuthenticationError("Not authenticated");
+          }
+          const deletedCategory = await Category.findOneAndDelete({ _id: _id });
+          const updatedProduct = await Product.updateMany(
+            { category: deletedCategory._id },
+            { $set: {category: null } }
+          );
+          console.log("deleted category :", deletedCategory._id);
+          return updatedProduct;
+        } catch (error) {
+          console.error("deletedCategory:", error);
+          throw error;
+        }
       }
-    }),
+    ),
 
     deleteOrder: async (parent, { _id }, context, info) => {
       try {
@@ -401,26 +497,27 @@ const resolvers = {
       }
     },
 
-    //all login pro 
-    
+    //all login pro
+
     login: async (parent, { email, password }) => {
       try {
         // console.log("login: ", email, password);
         // console.log("Received password:", password);
         const user = await User.findOne({ email }).populate("role");
-        
-        console.log('user:', user);
+
+        console.log("user:", user);
         if (!user) {
           throw new AuthenticationError("No profile with this email found!");
         }
 
-        const correctPw = await user.isCorrectPassword(password.trim()); 
-        console.log('correct password:', correctPw);
+        const correctPw = await user.isCorrectPassword(password.trim());
+        console.log("correct password:", correctPw);
         if (!correctPw) {
           throw new AuthenticationError("Incorrect password!");
         }
 
         const token = signToken(user);
+        console.log("logged in!");
         return { token, user };
       } catch (e) {
         console.error("login :", e);
